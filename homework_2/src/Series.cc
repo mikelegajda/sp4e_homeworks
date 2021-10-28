670582cd2568295e8e5f19e99e46f9c93f73e068
Chen <chenyang.wang@epfl.ch>
2021-10-28 14:13:06 +0200

    add header file of Series class

---
+++ homework_2/src/Series.cc
@@ -0,0 +1,13 @@
+#include "Series.hh"
+
+Series::Series(/* args */)
+{
+};
+
+Series::~Series()
+{
+};
+
+double Series::compute(unsigned int N){
+    return 0.0;
+}
---
+++ homework_2/src/Series.hh
@@ -0,0 +1,11 @@
+class Series
+{
+private:
+    /* data */
+public:
+    Series(/* args */);
+    ~Series();
+    virtual double compute(unsigned int N) = 0; 
+};
+
+
