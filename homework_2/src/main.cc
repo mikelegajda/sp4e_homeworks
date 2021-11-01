c46d4b83f936549bdd44a89f2ec326ba6d129c60
Chen <chenyang.wang@epfl.ch>
2021-11-01 20:00:44 +0100

    make file type configurable

--- homework_2/src/main.cc
+++ homework_2/src/main.cc
@@ -6,15 +6,27 @@
 #include <fstream>
 #include <iomanip>
 #include <stdexcept>
+#include <sstream>
 
 int main(int argc, char ** argv){
 
-    std::string seriesType = argv[1];
-    unsigned int N = std::stoi(argv[2]);
-    std::string outputType = argv[3];
+    // using the stringstream to parse arguments
+    std::string seriesType;
+    unsigned int N;
+    std::string outputType;
+    std::string fileType;
+
+    std::stringstream sstr;
+    for (int i=1; i<argc; i++){
+        sstr << argv[i] << " ";
+    }
+    sstr >> seriesType >> N >> outputType >> fileType;
+
+    // print parsed arguments
     std::cout << "Input series type: " << seriesType << std::endl;
     std::cout << "Input N: " << N << std::endl;
     std::cout << "Input outputType: " << outputType << std::endl;
+    std::cout << "Input fileType: " << fileType << std::endl;
 
     double ret = 0.0;
     Series *ptr = NULL;
@@ -26,21 +38,38 @@ int main(int argc, char ** argv){
     }
     else{
         std::cerr << "Unkown series type" << std::endl;
+        return -1;
     }
+
 
     DumperSeries *dumperPtr = NULL;
     if (outputType == "printToScreen"){
-        dumperPtr = new PrintSeries(*ptr, N, 10);
+        dumperPtr = new PrintSeries(*ptr, N, 1);
         dumperPtr->dump(std::cout);
     }else if (outputType == "saveToFile"){
-        dumperPtr = new WriteSeries(*ptr, N, 10);
+        std::string seperator;
+        if (fileType.empty() || fileType == "tsv"){
+            seperator = "\t";
+        }else if (fileType == "csv"){
+            seperator = ",";
+        }else if (fileType == "txt"){
+            seperator = "\t";
+        }else if (fileType == "psv"){
+            seperator = "|";
+        }else{
+            std::cerr << "Unkown file type" << std::endl;
+            return -1;
+        }
+        dumperPtr = new WriteSeries(*ptr, N, 1);
+        WriteSeries *writerPtr = static_cast<WriteSeries *>(dumperPtr);
         std::ofstream resultFile;
-        resultFile.open("result.txt");
-        dumperPtr->dump(resultFile);
+        resultFile.open("result." + fileType);
+        writerPtr->setSeparator(seperator);
+        writerPtr->dump(resultFile);
     }
     else{
         std::cerr << "Unkown output type" << std::endl;
-        // throw std::invalid_argument("Unkown output type");
+        return -1;
     };
 
     delete dumperPtr;
