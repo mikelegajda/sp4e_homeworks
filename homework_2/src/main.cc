92583a896b5200ea7d913fa6754fb8a4d5efd7c6
Chen <chenyang.wang@epfl.ch>
2021-11-02 21:25:03 +0100

    integrate RiemannIntegral to the main

--- homework_2/src/main.cc
+++ homework_2/src/main.cc
@@ -9,36 +9,46 @@
 #include <stdexcept>
 #include <sstream>
 #include "dumper__series.hh"
+#include <math.h>
 
+// Usage example:
+// 1. ./src/main arithmetic 100 saveAllToFile csv
+// 2. ./src/main pi 100 printAllToScreen
+// 3. ./src/main integral 10000 printOnlyResult nan cos 0 3.1415926
+// 4. ./src/main integral 10000 printOnlyResult nan cubic 0 1
 
 int main(int argc, char ** argv){
-    /* Riemann Test
-    Series *ptr = NULL;
-    double ret = 0.0;
-    std::string f_type = "cos";
-    ptr = new RiemannIntegral(0, 1, 4, f_type);
-    DumperSeries *dumperPtr = NULL;
-    dumperPtr = new PrintSeries(*ptr, 100, 1);
-*/
-
+    //Riemann Test
+    // Series *ptr = NULL;
+    // double ret = 0.0;
+    // std::string f_type = "sin";
+    // ptr = new RiemannIntegral(0, M_PI/2.0, f_type);
+    // ret = ptr->compute(1000000);
+    // std::cout<< ret << std::endl;
 
     // using the stringstream to parse arguments
     std::string seriesType;
     unsigned int N;
     std::string outputType;
     std::string fileType;
+    std::string functionType;
+    double a;
+    double b;
 
     std::stringstream sstr;
     for (int i=1; i<argc; i++){
         sstr << argv[i] << " ";
     }
-    sstr >> seriesType >> N >> outputType >> fileType;
+    sstr >> seriesType >> N >> outputType >> fileType >> functionType >> a >> b;
 
     // print parsed arguments
     std::cout << "Input series type: " << seriesType << std::endl;
     std::cout << "Input N: " << N << std::endl;
     std::cout << "Input outputType: " << outputType << std::endl;
     std::cout << "Input fileType: " << fileType << std::endl;
+    std::cout << "Input functionType: " << functionType << std::endl;
+    std::cout << "Input integral lower limit a: " << a << std::endl;
+    std::cout << "Input integral upper limit b: " << b << std::endl;
 
     double ret = 0.0;
     Series *ptr = NULL;
@@ -47,6 +57,8 @@ int main(int argc, char ** argv){
         ptr = new ComputeArithmetic();
     }else if (seriesType == "pi"){
         ptr = new ComputePi();
+    }else if (seriesType == "integral"){
+        ptr = new RiemannIntegral(a, b, functionType);
     }
     else{
         std::cerr << "Unkown series type" << std::endl;
@@ -55,14 +67,15 @@ int main(int argc, char ** argv){
 
 
     DumperSeries *dumperPtr = NULL;
-    if (outputType == "printToScreen"){
+    if (outputType == "printAllToScreen" && seriesType != "integral"){
         dumperPtr = new PrintSeries(*ptr, N, 1);
         PrintSeries *printerPtr = static_cast<PrintSeries *>(dumperPtr);
         printerPtr->dump();
-    }else if (outputType == "saveToFile"){
+    }else if (outputType == "saveAllToFile" && seriesType != "integral"){
         std::string seperator;
-        if (fileType.empty() || fileType == "tsv"){
+        if (fileType.empty()){
             seperator = "\t";
+            fileType = "txt";
         }else if (fileType == "csv"){
             seperator = ",";
         }else if (fileType == "txt"){
@@ -80,6 +93,9 @@ int main(int argc, char ** argv){
         writerPtr->setSeparator(seperator);
         // writerPtr->dump(resultFile);
         resultFile << *writerPtr; // using defined operator
+    }else if(outputType == "printOnlyResult" && seriesType == "integral"){
+        std::cout<< "Integral Result: " << std::endl;
+        std::cout<< ptr->compute(N) << std::endl;
     }
     else{
         std::cerr << "Unkown output type" << std::endl;
