d0f2ed9caa579a0078058a12fed787962cf5ca00
mikelegajda <mikelegajda@gmail.com>
2021-12-11 18:22:29 +0100

    Init generate distribution script

---
+++ homework_3/src/generate_distribution.py
@@ -0,0 +1,15 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Parse inputs: shape, xDomain, yDomain, radius
+
+# create coordinate grid
+
+# set force, velocity, mass, temperature
+
+# Boundary conditions for temperature
+
+# Output
+
