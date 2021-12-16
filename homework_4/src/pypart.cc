8dd28e7d4d9529b2be66d3649f149cfe3a8d82aa
Chen <chenyang.wang@epfl.ch>
2021-12-16 19:42:33 +0100

    add python bindings for factory interface classes

--- homework_4/src/pypart.cc
+++ homework_4/src/pypart.cc
@@ -15,4 +15,29 @@ PYBIND11_MODULE(pypart, m) {
   m.doc() = "pybind of the Particles project";
 
   // bind the routines here
+  // wrap the class
+  py::class_<ParticlesFactoryInterface>(
+      m, "ParticlesFactoryInterface",
+      py::dynamic_attr() // to allow new members to be created dynamically
+      )
+      // .def(py::init()) // no python constructor
+      .def("getInstance", &ParticlesFactoryInterface::getInstance) // getInstance method
+      .def("createSimulation", &ParticlesFactoryInterface::createSimulation); // createSimulation method
+
+
+  py::class_<MaterialPointsFactory, ParticlesFactoryInterface>(
+      m, "MaterialPointsFactory",
+      py::dynamic_attr() // to allow new members to be created dynamically
+      )
+      // .def(py::init()) // no python constructor
+      .def("getInstance", &MaterialPointsFactory::getInstance) // getInstance method
+      .def("createSimulation", &MaterialPointsFactory::createSimulation); // createSimulation method
+  
+  py::class_<PlanetsFactory, ParticlesFactoryInterface>(
+      m, "PlanetsFactory",
+      py::dynamic_attr() // to allow new members to be created dynamically
+      )
+      // .def(py::init()) // no python constructor
+      .def("getInstance", &PlanetsFactory::getInstance) // getInstance method
+      .def("createSimulation", &PlanetsFactory::createSimulation); // createSimulation method
 }
