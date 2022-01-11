e0ee826d254142cff98149d06ce127073a41cb29
Chen <chenyang.wang@epfl.ch>
2022-01-12 00:05:26 +0100

    add sinHeat test

--- homework_4/src/test_temperature.cc
+++ homework_4/src/test_temperature.cc
@@ -1,81 +1,77 @@
+#include "compute_temperature.hh"
+#include "compute_temperature_finite_differences.hh"
 #include "csv_reader.hh"
 #include "csv_writer.hh"
 #include "material_point.hh"
 #include "material_points_factory.hh"
 #include "system.hh"
-#include "compute_temperature.hh"
 #include <gtest/gtest.h>
 #include <iostream>
-#include "compute_temperature_finite_differences.hh"
-
 
 #define TEST_TOLERANCE 1e-8
 
 /*****************************************************************/
 // Fixture class
 class TemperatureTest : public ::testing::Test {
-  public:
+public:
+  void SetUp() override {
+    MaterialPointsFactory::getInstance();
+    // Store particles in a vector
+    std::vector<MaterialPoint> vecParticles;
 
-      void SetUp() override {
-        MaterialPointsFactory::getInstance(); 
-        // Store particles in a vector
-        std::vector<MaterialPoint> vecParticles; 
-
-        // Populating vector with particles  
-        for (UInt i = 0; i < size; ++i){
-            for (UInt j = 0; j < size; ++j){
-                MaterialPoint p;
-                // Evenly spaced particles 
-                p.getPosition()[0] = -L/2 + i*L/(size-1);
-                p.getPosition()[1] = -L/2 + j*L/(size-1); 
-                // Fix z-position to 0
-                p.getPosition()[2] = 0; 
-                p.getTemperature() = T;
-                p.getHeatSource() = 0;
-                vecParticles.push_back(p); 
-                T++;
-            }
-      }
-
-      // Add created particles to vecParticles
-      for (auto& p : vecParticles) {
-        system.addParticle(std::make_shared<MaterialPoint>(p)); 
+    // Populating vector with particles
+    for (UInt i = 0; i < size; ++i) {
+      for (UInt j = 0; j < size; ++j) {
+        MaterialPoint p;
+        // Evenly spaced particles
+        p.getPosition()[0] = -L / 2 + i * L / (size - 1);
+        p.getPosition()[1] = -L / 2 + j * L / (size - 1);
+        // Fix z-position to 0
+        p.getPosition()[2] = 0;
+        p.getTemperature() = T;
+        p.getHeatSource() = 0;
+        vecParticles.push_back(p);
+        T++;
       }
     }
 
-    System system;
+    // Add created particles to vecParticles
+    for (auto& p : vecParticles) {
+      system.addParticle(std::make_shared<MaterialPoint>(p));
+    }
+  }
 
-    // Set variables
-    Real dt = 0.001;
-    Real  rho = 1.;
-    Real  C =  1.;
-    Real  k = 1.;
-    UInt size = 3;
-    Real L = 2.;
-    // Set initial temperature field
-    Real  T = 1;
+  System system;
 
+  // Set variables
+  Real dt = 0.001;
+  Real rho = 1.;
+  Real C = 1.;
+  Real k = 1.;
+  UInt size = 3;
+  Real L = 2.;
+  // Set initial temperature field
+  Real T = 1;
 };
 /*****************************************************************/
 
 TEST_F(TemperatureTest, temperature_equilibrium) {
 
-  //Number of allowed iterations
+  // Number of allowed iterations
   UInt steps = 20000;
   dt = 0.001;
 
-  ComputeTemperatureFiniteDifferences temperature = ComputeTemperatureFiniteDifferences(dt);
+  ComputeTemperatureFiniteDifferences temperature =
+      ComputeTemperatureFiniteDifferences(dt);
 
   // Parameters
-  temperature.getCapacity()= C;
+  temperature.getCapacity() = C;
   temperature.getConductivity() = k;
   temperature.getDensity() = rho;
   temperature.getDeltat() = dt;
   temperature.getL() = L;
 
-  for(UInt i = 0; i < steps; ++i){
-    temperature.assembleRightHandSide(system);
-    temperature.assembleLinearOperator(system);
+  for (UInt i = 0; i < steps; ++i) {
     temperature.compute(system);
   }
 
@@ -84,3 +80,40 @@ TEST_F(TemperatureTest, temperature_equilibrium) {
     ASSERT_NEAR(p.getTemperature(), 0, TEST_TOLERANCE);
   }
 }
+
+/*****************************************************************/
+TEST_F(TemperatureTest, sinHeat) {
+  // Number of allowed iterations
+  UInt steps = 1000;
+
+  // assume L = 2
+  for (auto& p : system) {
+    MaterialPoint& pt = dynamic_cast<MaterialPoint&>(p);
+    // initial temperature at equilibrium state
+    pt.getTemperature() = sin(M_PI * pt.getPosition()[0]);
+    pt.getHeatSource() = (M_PI) * (M_PI)*sin(M_PI * pt.getPosition()[0]);
+  }
+
+  
+
+  auto temperature = std::make_shared<ComputeTemperatureFiniteDifferences>(dt);
+  // Parameters
+  temperature->getCapacity() = C;
+  temperature->getConductivity() = k;
+  temperature->getDensity() = rho;
+  temperature->getL() = L;
+
+  for (UInt s = 0; s < steps; ++s) {
+    temperature->compute(system);
+  }
+
+  for (UInt j = 0; j < size; j++) {
+    for (UInt i = 0; i < size; i++) {
+      MaterialPoint& pt =
+          dynamic_cast<MaterialPoint&>(system.getParticle(j * size + i));
+      Real x = pt.getPosition()[0];
+      ASSERT_NEAR(pt.getTemperature(), sin(x * M_PI), TEST_TOLERANCE);
+    }
+  }
+}
+/*****************************************************************/
